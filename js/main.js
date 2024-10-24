7bed2522303f37f6cd014125cf7c87f42b9a790c
Mateo Viglioni <teoviglioni@gmail.com>
2024-10-24 19:14:37 -0300

    Ajuste 3 de reproductor

--- js/main.js
+++ js/main.js
@@ -4,7 +4,35 @@
 
 //?--------------------------------------- Reproductor.
 
+// Aquí va tu JavaScript
+const iframe = document.getElementById('twitch-iframe');
+const playButton = document.getElementById('reproducir');
+const pauseButton = document.getElementById('pausar');
+const resetButton = document.getElementById('reiniciar');
+const volumeControl = document.getElementById('volumen');
 
+const player = new Twitch.Player('twitch-iframe', {
+    channel: 'radiomasuytv',
+    parent: ['radio-mas.netlify.app'],
+});
+
+playButton.addEventListener('click', () => {
+    player.play();
+});
+
+pauseButton.addEventListener('click', () => {
+    player.pause();
+});
+
+resetButton.addEventListener('click', () => {
+    player.seek(0);
+    player.play();
+});
+
+volumeControl.addEventListener('input', (event) => {
+    const volume = event.target.value / 100;
+    player.setVolume(volume);
+});
 
 //?--------------------------------------- Reproductor.
 
